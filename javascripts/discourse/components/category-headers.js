f5ddb046f9e80c576d8b5ecd334c67bc6886a8ee
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-06-27 14:51:15 +0800

    Update category-headers.js

--- javascripts/discourse/components/category-headers.js
+++ javascripts/discourse/components/category-headers.js
@@ -2,7 +2,7 @@ import icon from "discourse/helpers/d-icon";
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 
-export default class CategoryHeader extends Component {
+export default class CategoryHeaders extends Component {
   @service siteSettings;
   
   get ifParentCategory() {
