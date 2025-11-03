52bb9164353ee35cb2f389280d28c4e60b55b20d
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-11-03 17:55:56 +0800

    Change outlet to discovery-list-controls-above

--- javascripts/discourse/api-initializers/init_banners.js
+++ javascripts/discourse/api-initializers/init_banners.js
@@ -2,5 +2,5 @@ import { apiInitializer } from "discourse/lib/api";
 import CategoryHeader from "../components/category-header";
 
 export default apiInitializer((api) => {
-  api.renderInOutlet("above-category-heading", CategoryHeader);
+  api.renderInOutlet("discovery-list-controls-above", CategoryHeader);
 });
